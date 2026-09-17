220dece91a12bb2e19c5e0166d53745c9f1ca192
Francesco Doria <doria5@live.it>
2026-09-17 20:26:13 +0200

    nav styling

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,12 +2,15 @@ import { Link } from "@tanstack/react-router";
 
 export const Nav = () => {
     return (
-        <nav>
-            <Link to="/" activeOptions={{ exact: true }}>
-                Home
-            </Link>{" "}
-            <Link to="/about">About</Link>
-            <Link to="/garden">My Garden</Link>
-        </nav>
+        <div id="nav-container">
+            <span>Tamarix</span>
+            <nav>
+                <Link to="/" activeOptions={{ exact: true }}>
+                    Home
+                </Link>{" "}
+                <Link to="/about">About</Link>
+                <Link to="/garden">My Garden</Link>
+            </nav>
+        </div>
     );
 };
--- src/routes/garden.tsx
+++ src/routes/garden.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute("/garden")({
     component: RouteComponent,
     head: () => ({
         meta: [{ title: "My Garden" }],
-    }),
+    })
 });
 
 function RouteComponent() {
